1447f334afaa754beca7e082fe4819f8f234d5e7
yasminee99 <64501536+yasminee99@users.noreply.github.com>
2020-05-15 17:59:57 +0200

    first committ

---
+++ ProjetNadhamni/Parents.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetNadhamni
+{
+    public partial class Parents : Form
+    {
+        public Parents()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaveParents_Click(object sender, EventArgs e)
+        {
+            if (PersonalInformation.Worker)
+            {
+                Job j = new Job();
+                j.Show();
+                this.Hide();
+            }
+            if (PersonalInformation.Student)
+            {
+                Studies st = new Studies();
+                st.Show();
+                this.Hide();
+            }
+            if (PersonalInformation.unemployed)
+            {
+                Dashboard d = new Dashboard();
+                d.Show();
+                this.Hide();
+            }
+        }
+    }
+}
---
+++ ProjetNadhamni/Tasks.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetNadhamni
+{
+    public partial class Tasks : Form
+    {
+        public Tasks()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_dashboard_Click(object sender, EventArgs e)
+        {
+            Dashboard dsh2 = new Dashboard();
+            this.Hide();
+            dsh2.Show();
+        }
+
+        private void btn_profile_Click(object sender, EventArgs e)
+        {
+            Profile pf2 = new Profile();
+            this.Hide();
+            pf2.Show();
+        }
+
+        private void btn_statistics_Click(object sender, EventArgs e)
+        {
+            Statistics stc2 = new Statistics();
+            this.Hide();
+            stc2.Show();
+        }
+
+        private void btn_parameters_Click(object sender, EventArgs e)
+        {
+            Settings st2 = new Settings();
+            this.Hide();
+            st2.Show();
+        }
+
+        private void ExitDashboard_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void cmb_category_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
